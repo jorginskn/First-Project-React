c2dcc03659c732be72acfae82aeef105103ba89a
jorginskn <jorgepmbr@gmai.com>
2021-09-06 16:08:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Tasks from './components/Tasks';
+
+import './App.css';
+import AddTask from "./components/AddTasks";
+
+const App = () => {
+  // let message = "Hello World!";
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar programação',
+      completed:false,
+
+    },
+    {
+      id: '2',
+      title: 'Ler livros',
+      completed: true,
+    },
+    
+  ]);
+ 
+
+    return (
+    <>
+     
+      <div className="container">
+         <AddTask/>
+        <Tasks tasks={tasks}/>
+      </div>   
+   </>
+  );
+};
+export default App;
